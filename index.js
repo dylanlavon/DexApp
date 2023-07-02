621b7496a7707445c70f732a0dfbfd80862e2d33
Dylan Britain <dylanlavon@gmail.com>
2023-07-02 10:30:50 -0500

    Set up bootstrap

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const pokemon = require('pokemontcgsdk');
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 
 
