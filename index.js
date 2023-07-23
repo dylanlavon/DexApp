7317efa8670c6926cdbade23cb3ea37c9b318fb2
Dylan Britain <dylanlavon@gmail.com>
2023-07-22 19:07:23 -0500

    Fix set page ordering by ordering setMap / idMap. Clean up code and add comments.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Set up requirements
 const pokemon = require('pokemontcgsdk');
 const express = require('express');
 const app = express();
@@ -6,46 +7,79 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 
-
 const API_KEY = '9926f1ab-1b23-4897-9311-c7266b6ca406'
 const PORT = 3000
 
 pokemon.configure({apiKey: API_KEY})
 app.set('view engine', 'ejs')
-
+ 
+// HOME PAGE
 app.get('/', (req, res) => {
     res.send("Welcome home!")
 })
 
+// ABOUT PAGE
 app.get('/about', (req, res) =>
 {
     res.send("This is the About page!")   
 })
 
+// SET PAGE
 app.get('/set', (req, res) =>
 {
     pokemon.set.all()
         .then((set) => {
-            console.log(set[0].name)
         // Create hashmap for Series:set pairs
         const setMap = {}; 
         const idMap = {};
+
         set.forEach(item => {
-        const { series, name, id } = item; 
+        const { series, name, id, releaseDate} = item; 
+
+        // Convert releaseDate to a valid date object
+        const dateParts = releaseDate.split('/');
+        const validDate = new Date(`${dateParts[0]}-${dateParts[1]}-${dateParts[2]}`);
+
         
         if(setMap.hasOwnProperty(series)) { 
-            setMap[series].push(name); 
-            idMap[series].push(id);
+            setMap[series].push({ name, releaseDate: validDate }); // Store name and converted date 
+            idMap[series].push({ id, releaseDate: validDate }); // Store name and converted date 
         } else { 
-            setMap[series] = [name]; 
-            idMap[series] = [id];
-        } 
+            setMap[series] = [{ name, releaseDate: validDate }]; // Store name and converted date as an object 
+            idMap[series] = [{ id, releaseDate: validDate }]; // Store name and converted date as an object 
+        }
         }); 
 
-    res.render('set', {setInfo: set, setMap: setMap, idMap: idMap, page: "set"}); 
+        // Convert setMap to an array of key-value pairs and sort it based on the earliest release date
+        const sortedSetMapArray = Object.entries(setMap).sort(([, a], [, b]) => {
+            const earliestSetA = a.reduce((earliest, set) => set.releaseDate < earliest ? set.releaseDate : earliest, new Date());
+            const earliestSetB = b.reduce((earliest, set) => set.releaseDate < earliest ? set.releaseDate : earliest, new Date());
+            return earliestSetA - earliestSetB;
+        });
+
+        // Convert idMap to an array of key-value pairs and sort it based on the earliest release date
+        const sortedIdMapArray = Object.entries(idMap).sort(([, a], [, b]) => {
+            const earliestSetA = a.reduce((earliest, set) => set.releaseDate < earliest ? set.releaseDate : earliest, new Date());
+            const earliestSetB = b.reduce((earliest, set) => set.releaseDate < earliest ? set.releaseDate : earliest, new Date());
+            return earliestSetA - earliestSetB;
+        });
+
+        // Convert the sorted array back into the sorted setMap object
+        const sortedSetMap = Object.fromEntries(sortedSetMapArray);
+        const sortedIdMap = Object.fromEntries(sortedIdMapArray);
+
+        // Sort the setMap based on releaseDate
+        for (const series in sortedSetMap) 
+        {
+            sortedSetMap[series].sort((a, b) => a.releaseDate - b.releaseDate);
+            sortedIdMap[series].sort((a, b) => a.releaseDate - b.releaseDate);
+        }
+
+    res.render('set', {setInfo: set, setMap: sortedSetMap, idMap: sortedIdMap, page: "set"}); 
   })
 })
 
+// SETLIST PAGE
 app.get('/set/:setId', (req, res) =>
 {
     const setId = req.params.setId;
@@ -67,22 +101,15 @@ app.get('/set/:setId', (req, res) =>
 
 })
 
-app.get('/card/:cardId', (req, res) =>
-{
+// CARD PAGE
+app.get('/card/:cardId', (req, res) => {
     pokemon.card.find(req.params.cardId)
         .then(card => {
-    res.render('card', {cardInfo: card, page: "card"}) 
-    
-    // console.log(card)
-})
-    
-})
+            res.render('card', { cardInfo: card, page: "card" });
+        });
+});
 
-// pokemon.card.find('base1-4')
-// .then(card => {
-//     console.log(card) // "Charizard"
-// })
-
+// Listen on PORT 3000
 app.listen(PORT, () => 
 {
     console.log("App is listening on port ", PORT)
